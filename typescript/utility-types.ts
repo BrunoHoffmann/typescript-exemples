2e6efbe6c62e7c16208213b59d0ba2c644a48606
Bruno Hoffmann <bruno.hoffmann.ext@infracommerce.com.br>
2022-05-22 19:57:59 -0300

    utility types

---
+++ typescript/utility-types.ts
@@ -0,0 +1,42 @@
+interface User {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+  age?: number;
+}
+
+type UserReadonly = Readonly<User>; // deixa tudo readonly
+
+type UserRequired = Required<User>; // deixa tudo obrigatorio mesmo tendo ?: na property
+
+type UserPartial = Partial<User>; // deixa tudo opcional ou parcial
+
+type UserCredentials = Pick<User, 'email' | 'password'>; // vais pegar so email e password da interface
+
+type UserNo = Omit<User, 'email' | 'password'>; // vais pegar tods exceto email e password
+
+const user: UserPartial = {
+  name: 'John',
+  phone: '923012 039123',
+}
+
+interface Post {
+  title: string;
+  subtitle: string;
+}
+
+const POstsMappedBySections = {
+  sports: [
+    {
+      title: 'espanha',
+      subtitle: 'testa',
+    },
+  ],
+  nutrition: [
+    {
+      title: 'Fastfood',
+      subtitle: 'mcdonalts aa',
+    },
+  ],
+}
